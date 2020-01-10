052e631413bb605e329e1ea6d6056ddbc6910b3b
Domingo <domingomg97@gmail.com>
2020-01-10 22:46:31 +0000

    Commit inicial

---
+++ app.js
@@ -0,0 +1,18 @@
+// Requireds
+const argv = require('./config/yargs').argv;
+const colors = require('colors');
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite).then((archivo) => console.log(`Archivo creado: ${ archivo }`.green)).catch((error) => console.log(`${ error }`.red));
+        break;
+    default:
+        console.log('Comando desconocido');
+}
---
+++ config/yargs.js
@@ -0,0 +1,21 @@
+const opciones = {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+}
+
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', opciones)
+    .command('crear', 'Crear la tabla de multiplicar en un fichero .txt', opciones)
+    .help()
+    .argv;
+
+
+module.exports = {
+    argv
+}
